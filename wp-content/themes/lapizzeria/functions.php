d02bf37f574763c8e4a12416a5e809174030eaf5
knubbe82 <knubbe@Nikolas-iMac.local>
2018-01-03 19:07:04 +0100

    about us

--- wp-content/themes/lapizzeria/functions.php
+++ wp-content/themes/lapizzeria/functions.php
@@ -1,8 +1,16 @@
 <?php
 
+function lapizzeria_setup()
+{
+    add_theme_support('post-thumbnails');
+    add_image_size('boxes', 437, 291, true);
+}
+add_action('after_setup_theme', 'lapizzeria_setup');
+
 function lapizzeria_styles()
 {
     // adding stylesheets
+    wp_register_style('googlefont', 'https://fonts.googleapis.com/css?family=Open+Sans:400,700|Raleway:400,700,900', array(), '1.0.0');
     wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '7.0.0');
     wp_register_style('fontawesome', get_template_directory_uri() . '/css/font-awesome.css', array(), '4.7.0');
     wp_register_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0');
@@ -10,6 +18,7 @@ function lapizzeria_styles()
     // enqueue the style
     wp_enqueue_style('normalize');
     wp_enqueue_style('fontawesome');
+    wp_enqueue_style('googlefont');
     wp_enqueue_style('style');
 
     wp_register_script('script', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0', true);
--- wp-content/themes/lapizzeria/js/scripts.js
+++ wp-content/themes/lapizzeria/js/scripts.js
@@ -1,10 +1,11 @@
 $ = jQuery.noConflict();
 
 $(document).ready(function () {
+    // Menu Button
    $('.mobile-menu a').on('click', function () {
        $('nav.site-nav').toggle('slow');
    });
-
+    // Show the Mobile Menu
    var breakpoint = 768;
    $(window).resize(function () {
        if($(document).width() >= breakpoint) {
@@ -13,4 +14,11 @@ $(document).ready(function () {
            $('nav.site-nav').hide();
        }
    });
+   // Adapt the height of the images to the div elements
+    var images = $('.box-image');
+    var imageHeight = images[0].height;
+    var boxes = $('.content-box');
+    $(boxes).each(function(i, element) {
+        $(element).css({'height': imageHeight + 'px'});
+    });
 });
